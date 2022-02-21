91e5e384d3c65cc2ddb6ce7716d9294e86890683
Ninh Van <nhvan@go.olemiss.edu>
2022-02-21 15:19:32 -0600

    Update login component

--- Client/src/components/Login.js
+++ Client/src/components/Login.js
@@ -7,36 +7,42 @@ import logo from '../assets/logowhite.png'
 const Login = () => {
     return (
         <div className="flex justify-start items-center flex-col h-screen">
-            <div className="relative w-full h-full">
-                <video
-                    src={shareVideo}
-                    type="video/mp4"
-                    loop
-                    controls={false}
-                    muted
-                    autoPlay
-                    className="w-full h-full object-cover"
-                />
-                <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay"></div>
-                <div className="p-5">
-                    <img src={logo} width="130px" alt="logo" />
-                </div>
-                <div className="shadow-2xl">
-                    <GoogleLogin
-                        clientId=''
-                        render={(renderProps) => (
-                            <button
-                                type="button"
-                                className="bg-mainColor flex justfy-center items-center p-3 rounded-lg cursor-pointer outline-none"
-                                onClick={renderProps.onClick}
-                                disabled={renderProps.disabled}
-                            >
-                                <FcGoogle className="mr-4" /> Sign in with Google
-                            </button>
-                        )}/>
-                </div>
+        <div className=" relative w-full h-full">
+          <video
+            src={shareVideo}
+            type="video/mp4"
+            loop
+            controls={false}
+            muted
+            autoPlay
+            className="w-full h-full object-cover"
+          />
+  
+          <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+            <div className="p-5">
+              <img src={logo} width="130px" />
             </div>
+  
+            <div className="shadow-2xl">
+              <GoogleLogin
+                clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
+                render={(renderProps) => (
+                  <button
+                    type="button"
+                    className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                    onClick={renderProps.onClick}
+                    disabled={renderProps.disabled}
+                  >
+                    <FcGoogle className="mr-4" /> Sign in with google
+                  </button>
+                )}
+                cookiePolicy="single_host_origin"
+              />
+            </div>
+          </div>
         </div>
-    )
-}
-export default Login;
+      </div>
+    );
+  };
+  
+  export default Login;
