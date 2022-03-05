f4f6d2ed3a2b3d3ee6b1514e5987a09f41f2ccbc
Ninh Van <nhvan@go.olemiss.edu>
2022-03-04 20:28:48 -0600

    Frontend update and create necessary pages

--- Client/src/container/Home.js
+++ Client/src/container/Home.js
@@ -8,48 +8,51 @@ import { client } from '../client'
 import logo from '../assets/logo.png'
 import { userQuery } from "../utils/data";
 const Home = () => {
-    const [toggleSidebar,setToggleSidebar]= useState(false)
-    const [user,setUser]= useState(null)
+    const [toggleSidebar, setToggleSidebar] = useState(false)
+    const [user, setUser] = useState();
     const scrollRef = useRef(null)
-    const userInfo=localStorage.getItem('user')!=='undefined'? JSON.parse(localStorage.getItem('user')) : localStorage.clear()
-    useEffect(()=>{
-        const query= userQuery(userInfo?.googleId);
-        client.fetch(query).then((data)=>{
+    const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear()
+    useEffect(() => {
+        const query = userQuery(userInfo?.googleId);
+        client.fetch(query).then((data) => {
             setUser(data[0]);
         })
-    },[])
-    useEffect(()=>{
-        scrollRef.current.scrollTo(0,0)
-    },[])
+    }, [])
+    useEffect(() => {
+        scrollRef.current.scrollTo(0, 0)
+    }, [])
     return (
         <div className='flex bg-gray-50  md:flex-row flex-col h-screen transaction-height duration-75 ease-out  '>
             <div className="hidden md:flex h-screen flex-initial">
-                <SideBar user={user && user} closeToggle={setToggleSidebar}/>
+                <SideBar user={user && user} closeToggle={setToggleSidebar} />
             </div>
             <div className="flex md:hidden flex-row">
-        
-                <HiMenu fontSize={40} className="cursor-pointer" onClick={()=> setToggleSidebar(true)}></HiMenu>
-                <Link to="/">
-                    <img src={logo} alt ="logo" className="w-28"/>
-                </Link>
-                <Link to={`user-profile/$(user?._id}`}>
-                    <img src={user?.image} alt ="logo" className="w-28"/>
-                </Link>
-            </div>
-            {toggleSidebar && (<div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
-                <div className="absolute w-full flex justify-end items-center p-2">
-                    <AiFillCloseCircle fontSize={30} className="cursor-pointer" onClick={()=> setToggleSidebar(false)}/>
+                <div className="p-2 w-full flex flex-row justify-between items-center shadow-md ">
+                    <HiMenu fontSize={40} className="cursor-pointer" onClick={() => setToggleSidebar(true)}></HiMenu>
+                    <Link to="/">
+                        <img src={logo} alt="logo" className="w-28" />
+                    </Link>
+                    <Link to={`user-profile/$(user?._id}`}>
+                        <img src={user?.image} alt="logo" className="w-28" />
+                    </Link>
                 </div>
-                <SideBar user={user && user} closeToggle={setToggleSidebar}/>
-        </div>)}
-        <div className="pb-2 flex-1 h-screen overflow-y-scroll">
-            {/* <Route>
-                <Route path="/user-profile/:userId" element={<UserProfile/>}/>
-                <Route path="/*" element={<Pins user={user&&user}/>}/>
+                {
+                    toggleSidebar && (<div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+                        <div className="absolute w-full flex justify-end items-center p-2">
+                            <AiFillCloseCircle fontSize={30} className="cursor-pointer" onClick={() => setToggleSidebar(false)} />
+                        </div>
+                        <SideBar user={user && user} closeToggle={setToggleSidebar} />
+                    </div>)
+                }
+            </div>
+            <div className="pb-2 flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
+                <Routes>
+                    <Route path="/user-profile/:userId" element={<UserProfile />} />
+                    <Route path="/*" element={<Pins user={user && user} />} />
 
-            </Route> */}
-        </div>
-        </div>
+                </Routes>
+            </div>
+        </div >
     )
 }
 export default Home;
